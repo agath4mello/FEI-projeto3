7287983b9261bce4b39569901629c992b66355c5
Agatha Mello <56945414+agath4mello@users.noreply.github.com>
2023-11-09 00:47:02 +0000

    inserção da função alterar tarefa

--- main.c
+++ main.c
@@ -21,8 +21,9 @@ void displayMenu() {
     printf("2. Listar Tarefas\n");
     printf("3. Modificar Estado da Tarefa\n");
     printf("4. Deletar Tarefa\n");
-    printf("5. Sair\n");
-    printf("Escolha uma opção (1/2/3/4/5): ");
+    printf("5. Alterar Tarefa\n");
+    printf("6. Sair\n");
+    printf("Escolha uma opção (1/2/3/4/5/6): ");
 }
 
 int main() {
@@ -36,6 +37,12 @@ int main() {
         exit(1);
     }
 
+  // Ler tarefas do arquivo
+    while (fread(&taskList[numTasks], sizeof(struct Task), 1, file) == 1)
+    {
+      numTasks++;
+    }
+  
     while (1) {
         displayMenu();
         int choice;
@@ -106,6 +113,74 @@ int main() {
         } else if (choice == 4) {
             // Código para deletar tarefa (sem alterações necessárias)
         } else if (choice == 5) {
+            if (numTasks == 0) {
+                printf("A lista de tarefas está vazia.\n");
+            } else {
+                printf("Digite o número da tarefa a ser alterada (1-%d): ", numTasks);
+                int taskNumber;
+                scanf("%d", &taskNumber);
+
+                if (taskNumber >= 1 && taskNumber <= numTasks) {
+                    int fieldChoice;
+                    printf("Escolha o campo a ser alterado:\n");
+                    printf("1. Prioridade\n");
+                    printf("2. Descrição\n");
+                    printf("3. Categoria\n");
+                    printf("4. Estado\n");
+                    printf("Escolha uma opção (1/2/3/4): ");
+                    scanf("%d", &fieldChoice);
+
+                    if (fieldChoice >= 1 && fieldChoice <= 4) {
+                        struct Task *task = &taskList[taskNumber - 1];
+
+                        switch (fieldChoice) {
+                            case 1:
+                                // Alterar prioridade
+                                printf("Nova prioridade (0-10): ");
+                                scanf("%d", &task->priority);
+                                break;
+                            case 2:
+                                // Alterar descrição
+                                printf("Nova descrição (até 300 caracteres): ");
+                                scanf(" %[^\n]", task->description);
+                                break;
+                            case 3:
+                                // Alterar categoria
+                                printf("Nova categoria (até 100 caracteres): ");
+                                scanf(" %[^\n]", task->category);
+                                break;
+                            case 4:
+                                // Alterar estado
+                                printf("Novo estado (0 - NÃO INICIADO, 1 - EM ANDAMENTO, 2 - COMPLETO): ");
+                                int newStatus;
+                                scanf("%d", &newStatus);
+
+                                if (newStatus >= NAO_INICIADO && newStatus <= COMPLETO) {
+                                    task->status = newStatus;
+                                } else {
+                                    printf("Estado de tarefa inválido.\n");
+                                }
+                                break;
+                            default:
+                                printf("Opção inválida. Tente novamente.\n");
+                        }
+
+                        printf("Tarefa alterada com sucesso.\n");
+
+                        // Atualizar o arquivo com as tarefas alteradas
+                        fseek(file, 0, SEEK_SET);
+                        for (int i = 0; i < numTasks; i++) {
+                            fwrite(&taskList[i], sizeof(struct Task), 1, file);
+                        }
+                    } else {
+                        printf("Opção de campo inválida. Tente novamente.\n");
+                    }
+                } else {
+                    printf("Número de tarefa inválido.\n");
+                }
+            }
+        } else if (choice == 6) {
+            // Fechar o arquivo e sair
             fclose(file);
             break;
         } else {
